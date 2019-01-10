0c23ce49fc2c1ab16059a448d1041f529033acb2
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-01-10 20:16:02 +0800

    flask微服务与头条搜索

---
+++ flask微服务/test.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+import requests
+
+def test4():
+    data = {
+        'keyword': '盗梦空间'
+    }
+    url = 'http://xxx:6666/index/spider/toutiao/search/'
+    body = requests.post(url,data=data)
+    print(body.content.decode())
+
+if __name__ == "__main__":
+    test()
