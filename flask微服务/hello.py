64b8532b1b8a7b3c413a5e4dd67c1d4977223909
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-01-10 20:06:23 +0800

    flask微服务与头条搜索

--- flask微服务/hello.py
+++ flask微服务/hello.py
@@ -4,6 +4,10 @@ from flask import Flask
 from flask import request
 from toutiao_movie_search_test import *
 
+'''
+利用Python flask模块实现简单的后台传参触发进行头条搜索
+'''
+
 app = Flask(__name__)
 
 @app.route('/index/spider/toutiao/search/', methods=['GET', 'POST'])
@@ -12,9 +16,9 @@ def spider():
         keyword = request.values.get('keyword')
         m = Movie()
         m.movie_page_search(keyword)
-        return '您所要搜索的电影名称：{}'.format(keyword)
+        return '您所要搜索的关键字是：{}'.format(keyword)
     else:
         return 'other requests'
 
 if __name__ == '__main__':
-    app.run(port=8006,debug=True,host='0.0.0.0')
+    app.run(port=6666,debug=True,host='0.0.0.0')
