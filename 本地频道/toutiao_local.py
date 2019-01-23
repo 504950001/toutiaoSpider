bdc77d8f041008e9ff91ca6465d592cdfb2c163c
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-01-23 20:38:05 +0800

    今日头条本地频道文章内容抓取

--- 本地频道/toutiao_local.py
+++ 本地频道/toutiao_local.py
@@ -109,7 +109,7 @@ class Local(object):
             #当前请求Unix时间戳
             mt = int(time.time())
             #API签名字符串
-            para = 'b#28ac3c1abc' + 'juejinchain.com' + str(mt)
+            para = 'xxx' + 'xxx.com' + str(mt)
             sign = hashlib.md5(para.encode(encoding='UTF-8')).hexdigest()
             #媒体ID(即用户ID)
             mid = 0
